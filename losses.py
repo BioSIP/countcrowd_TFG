d046d0c24898f9ef3f38dcab72cbcec8e87b6c5f
pakitochus <pakitochus@gmail.com>
2021-04-30 17:37:21 +0200

    Added reduction keyword

--- losses.py
+++ losses.py
@@ -27,29 +27,41 @@ class RMSLELoss(nn.Module):
         return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(actual + 1)))
 
 class LogCoshLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super().__init__()
+        self.reduction = reduction
 
     def forward(self, y_t, y_prime_t):
         ey_t = y_t - y_prime_t
-        return torch.mean(torch.log(torch.cosh(ey_t + 1e-12)))
+        val =  torch.sum(torch.log(torch.cosh(ey_t + 1e-12)))
+        if self.reduction=='mean':
+            val /= y_t.shape[0]
+        return val
 
 class XTanhLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super().__init__()
+        self.reduction = reduction
 
     def forward(self, y_t, y_prime_t):
         ey_t = y_t - y_prime_t
-        return torch.mean(ey_t * torch.tanh(ey_t))
+        val = torch.mean(ey_t * torch.tanh(ey_t))
+        if self.reduction=='mean':
+            val /= y_t.shape[0]
+        return val
 
 
 class XSigmoidLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super().__init__()
+        self.reduction = reduction
 
     def forward(self, y_t, y_prime_t):
         ey_t = y_t - y_prime_t
-        return torch.mean(2 * ey_t / (1 + torch.exp(-ey_t)) - ey_t)
+        val = torch.sum(2 * ey_t / (1 + torch.exp(-ey_t)) - ey_t)
+        if self.reduction=='mean':
+            val /= y_t.shape[0]
+        return val
 
 
  
